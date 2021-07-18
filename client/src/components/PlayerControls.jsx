f1364c1011363f8f436d65e7c56e569fc53d420c
M-A-Justice <michae1jus7ice@gmail.com>
2021-07-18 12:46:13 -0400

    implemented functional start and play buttons

--- client/src/components/PlayerControls.jsx
+++ client/src/components/PlayerControls.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable max-len */
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   Audio,
   ControlsContainer,
@@ -9,12 +10,18 @@ import {
   VolumeContainer,
   SliderContainer,
   Slider,
+  Play,
+  Pause,
 } from '../styles/PlayerControls.style';
+import {
+  isPlaying,
+} from '../actions/index';
 
 const PlayerControls = () => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const duration = useSelector((state) => state.duration);
   const currentTime = useSelector((state) => state.currentTime);
+  const isPlayingBool = useSelector((state) => state.isPlaying);
   const { playedSeconds, played } = currentTime;
 
   let percentage = 0;
@@ -22,20 +29,28 @@ const PlayerControls = () => {
     percentage = Math.floor(played * 100);
   }
 
-  const formatTime = (time) => `${Math.floor(time / 60)}:${(time % 60).toString().length > 1 ? (time % 60) : '0'.concat((time % 60).toString())}`;
+  const formatTime = (time) => `
+    ${Math.floor(time / 60)}:${(time % 60).toString().length > 1 ? (time % 60) : '0'.concat((time % 60).toString())}
+  `;
+
+  const handlePlayPause = () => {
+    dispatch(isPlaying());
+  };
 
   return (
     <Audio>
       <DurationSlider>
         <SliderContainer>
-          <Slider played={percentage} duration={duration} playedSeconds={playedSeconds} />
+          <Slider played={percentage} duration={duration} playedSeconds={playedSeconds} isPlaying={isPlayingBool} />
         </SliderContainer>
         <DurationContainer>
           {`${formatTime(typeof playedSeconds !== 'number' ? 0 : Math.floor(playedSeconds))}/${formatTime(Math.floor(duration))}`}
         </DurationContainer>
       </DurationSlider>
       <ControlsContainer>
-        <ButtonsContainer />
+        <ButtonsContainer>
+          {isPlayingBool === true ? <Pause onClick={handlePlayPause} /> : <Play onClick={handlePlayPause} />}
+        </ButtonsContainer>
         <VolumeContainer />
       </ControlsContainer>
     </Audio>
--- client/src/styles/PlayerControls.style.js
+++ client/src/styles/PlayerControls.style.js
@@ -4,6 +4,8 @@ export const Audio = styled.div`
 `;
 
 export const ControlsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 6fr 2fr;
   background-color: #BDD5EA;
   height: 100%;
 `;
@@ -28,15 +30,15 @@ export const VolumeContainer = styled.div`
 
 export const SliderContainer = styled.div`
   height: 5px;
-  background-color: #577399;
+  background-color: #495867;
 `;
 
 const slider = (played) => keyframes`
   0% {
-    border-left: ${Math.floor(played)}vw solid black;
+    border-left: ${played}vw solid #577399;
   }
   100% {
-    border-left: 100vw solid black;
+    border-left: 100vw solid #577399;
   }
 `;
 
@@ -45,4 +47,27 @@ export const Slider = styled.div`
   width: 5px;
   background-color: white;
   animation: ${({ played }) => slider(played)} ${({ duration, playedSeconds }) => Math.floor(duration - playedSeconds)}s linear;
+  animation-play-state: ${({ isPlaying }) => (isPlaying === true ? 'running' : 'paused')}
+`;
+
+export const Play = styled.button`
+  box-sizing: border-box;
+  width: 74px;
+  height: 74px;
+  border-style: solid;
+  border-width: 37px;
+  border-width: 37px 0px 37px 74px;
+  border-color: transparent transparent transparent #577399;
+  background-color: transparent;
+  margin-left: 65%;
+`;
+
+export const Pause = styled.button`
+  width: 74px;
+  height: 74px;
+  border-style: double;
+  border-width: 0px 0px 0px 74px;
+  border-color: #577399;
+  background-color: transparent;
+  margin-left: 65%;
 `;
